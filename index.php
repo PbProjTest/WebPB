23cd63ba37e541a814b6e8590f6ca7e4a0ecd27d
PbProjTest <ymaccx46@gmail.com>
2024-09-16 21:23:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h2>Welcome to the Home Page</h2>
+    <a href="login.php">Login</a> | <a href="register.php">Register</a>
+</body>
+</html>
